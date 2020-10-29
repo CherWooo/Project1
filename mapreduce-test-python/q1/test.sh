e12fa212a6731df242ade8ff8647f9d3ea11eacc
CherWooo <63425412+CherWooo@users.noreply.github.com>
2020-10-28 23:57:08 -0400

    Update test.sh

--- mapreduce-test-python/q1/test.sh
+++ mapreduce-test-python/q1/test.sh
@@ -13,7 +13,7 @@ Created on Sun Oct 25 01:12:44 2020
 /usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-2.9.2.jar \
 -file ../../q1/mapper.py -mapper ../../q1/mapper.py \
 -file ../../q1/reducer.py -reducer ../../q1/reducer.py \
--input /vtime/input/* -output /q1/output/
+-input /q1/input/* -output /q1/output/
 /usr/local/hadoop/bin/hdfs dfs -cat /q1/output/part-00000
 /usr/local/hadoop/bin/hdfs dfs -rm -r /q1/input/
 /usr/local/hadoop/bin/hdfs dfs -rm -r /q1/output/
